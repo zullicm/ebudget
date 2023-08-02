732ff1efa577ec93c780a98b8c4ee3c364cdd0e6
Christopher Marzulli <chriszulli15@gmail.com>
2023-08-02 10:32:49 -0400

    Render Troubleshooting

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Budget.create! (name: 'Car')
+Budget.create! (name: 'House')
+Budget.create! (name: 'CC')
+Budget.create! (name: 'Loan')
