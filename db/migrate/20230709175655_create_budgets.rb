83c6b7529f1bd31fb351e5e93413536a944e6505
Christopher Marzulli <chriszulli15@gmail.com>
2023-09-16 15:31:16 -0400

    User model created

--- db/migrate/20230709175655_create_budgets.rb
+++
@@ -1,9 +0,0 @@
-class CreateBudgets < ActiveRecord::Migration[7.0]
-  def change
-    create_table :budgets do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class BudgetTest < ActiveSupport::TestCase
+class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
