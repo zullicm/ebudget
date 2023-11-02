709361b803fa37754c4bcf653c5103983742bedc
Christopher Marzulli <chriszulli15@gmail.com>
2023-11-02 13:29:58 -0400

    Added simple controller routes, created migrations

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,2 +1,14 @@
 class BudgetsController < ApplicationController
+
+  def index
+    budget = Budget.all
+    render json: budget
+  end
+
+  def create
+    budget BUdget.create(budget_params)
+    render json: budget, status: :created
+  end
+
+
 end
