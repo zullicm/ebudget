3413a349dd3767db3a7fdc32ee190fb3e5fa4d2e
Christopher Marzulli <chriszulli15@gmail.com>
2023-11-06 13:43:42 -0500

    Editing NavBar

--- client/src/Component/NavBar.js
+++ client/src/Component/NavBar.js
@@ -2,9 +2,17 @@ import React from "react";
 
 function NavBar() {
 
+  function navTest(word){
+    console.log(word)
+  }
+
   return(
     <div id="NavBar">
-      <h1 id="eBudget-Logo">eBudget</h1>
+      <div onClick={() => navTest("eBudget Logo")} className="eBudget-Logo">eBudget</div>
+      <div onClick={() => navTest()} className="item item-1">Item 1</div>
+      <div onClick={() => navTest()} className="item item-2">Item 2</div>
+      <div onClick={() => navTest()} className="item item-3">Item 3</div>
+      <div onClick={() => navTest("Profile")} className="item item-4">Profile</div>
     </div>
   )
 }
