a1275fe1b8d6d4de6560b4c285b0cccd610e5ad1
Christopher Marzulli <chriszulli15@gmail.com>
2023-11-06 12:47:47 -0500

    Installed and got React Router Working

---
+++ client/src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+
+  return(
+    <div>
+      <h1>Hello welcome to eBudget!</h1>
+    </div>
+  )
+}
+
+export default HomePage
---
+++ client/src/Pages/ReactLogo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+  function ReactLogo({ logo }) {
+
+    return(
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
+    )
+  }
+
+  export default ReactLogo
