03489ba8f1d9f5cbcae9c828b392178a13836435
Christopher Marzulli <chriszulli15@gmail.com>
2023-11-20 14:05:32 -0500

    Created sessions controller and creating login routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :expenses
   resources :categories
   resources :users
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
