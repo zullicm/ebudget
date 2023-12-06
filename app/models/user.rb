e6f5cb78feaf7e08af3dd9448a46a1cbc308952e
Christopher Marzulli <chriszulli15@gmail.com>
2023-12-06 11:54:20 -0500

    added relations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-
   has_secure_password
+  has_many :budgets
+  has_many :expenses
+  has_many :categories, through: :expenses
+
 
   has_many :budgets
   has_many :expenses
