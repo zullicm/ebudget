174bf52a1ef1d97ff66e1424da407740fec6786f
Christopher Marzulli <chriszulli15@gmail.com>
2023-12-14 14:29:35 -0500

    creating budget form

--- client/src/Component/BudgetForm.js
+++ client/src/Component/BudgetForm.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BudgetForm(){
+  const [amount, setAmount] = useState('')
+  
 
   return(
     <div>
-      <form>
-        
-      </form>
+      <p>Budget Amount {parseFloat(amount.replace(/,/g, '')) ? <p></p> : <p>Must be a number</p> }</p>
+      
+      <input
+      name="amount"
+      value={amount}
+      onChange={e => setAmount(e.target.value)}
+      >
+      </input>
     </div>
   )
 }
 
 export default BudgetForm
+
+
+
+  // :amount, :color, :start_date, :end_date, :user_id
+  // for now start and end date will look like this "03/23/2023" and will change once calender component is created
+
+  // parseFloat(amount.replace(/,/g, '') HOW TO CREATE NUM
