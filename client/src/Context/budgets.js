7ea68264e459780346ad486049377d8bf252a6df
Christopher Marzulli <chriszulli15@gmail.com>
2023-12-15 16:18:51 -0500

    Create budget context

---
+++ client/src/Context/budgets.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+const BudgetsContext = React.createContext();
+
+
+function BudgetsProvider({ children }) {
+  const [budgets, setBudgets] = useState(null)
+
+  useEffect(() =>{
+    fetch('/budgets')
+    .then(res => res.json())
+    .then(data => setBudgets(data))
+  },[])
+
+
+  return (
+    <BudgetsContext.Provider value={{budgets, setBudgets}}>
+      {children}
+    </BudgetsContext.Provider>
+  );
+}
+
+export { BudgetsContext, BudgetsProvider };
