d91eadb52df71a2d92d40429f3729b860a54d177
Christopher Marzulli <chriszulli15@gmail.com>
2023-12-18 12:04:57 -0500

    Added tailwins css

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -4,7 +4,7 @@ function HomePage() {
 
   return(
     <div>
-      <h1 id="welcome-messege">Hello welcome to eBudget!</h1>
+      <h1 id="welcome-messege" className="text-6xl">Hello welcome to eBudget!</h1>
     </div>
   )
 }
