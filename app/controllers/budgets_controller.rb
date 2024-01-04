215f656dfc091b3960e0fa070827ed340bba1189
Christopher Marzulli <chriszulli15@gmail.com>
2024-01-04 12:06:18 -0500

    Refactoring budget controller to better allow for error handling using active record

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -1,10 +1,20 @@
 class BudgetsController < ApplicationController
-
+  skip_before_action :authorized
   def index
-    budgets = Budget.where(user_id: session[:user_id]) 
-    render json: budgets, include: ['user']
+    budgets = Budget.where(user_id: session[:user_id])
+    if budgets
+      render json: budgets, include: ['user']
+    else
+      render_not_found_response
+    end
   end
 
+  def show
+    budget = find_budget
+    render json: budget
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
+  end
 
   def create
     budget = Budget.create(budget_params)
@@ -16,19 +26,21 @@ class BudgetsController < ApplicationController
   end
 
   def update
-    budget = Budget.find_by(id: params[:id])
-    budget.update(budget_params)
-    render json: budget
+    budget = find_budget
+    if budget
+      budget.update(budget_params)
+      render json: budget
+    else
+      render_not_found_response
+    end
   end
 
   def destroy
-    budget = Budget.find_by(id: params[:id])
-    if budget
-      budget.destroy
-      render json: budget 
-    else
-      render json: { error: "Budget not found" }, status: :not_found
-    end
+    budget = find_budget
+    budget.destroy
+    render json: budget 
+  rescue ActiveRecord::RecordNotFound
+    render_not_found_response
   end
 
   private
@@ -37,4 +49,17 @@ class BudgetsController < ApplicationController
     params.permit(:name, :amount, :color, :start_date, :end_date, :user_id)
   end
 
+  def find_budget
+    Budget.find(params[:id])
+  end
+  
+  def render_not_found_response
+    render json: { error: "Budget not found" }, status: :not_found
+  end
+
 end
+
+# When using the find instead of find_by method, we are thrown an active
+# record not found reponse instead of nil if it cant find the specific budget
+# So you can use the "rescue ActiveRecord::RecordNotFound" and that will go 
+# off if at any point there is an error
