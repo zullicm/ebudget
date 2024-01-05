f6800f0141bd6583082a38060283a8c6565519d0
Christopher Marzulli <chriszulli15@gmail.com>
2024-01-05 12:54:11 -0500

    Creating category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,43 @@
 class CategoriesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
+  def show
+    render json: find_category
+  end
+
+  def create
+    category = Category.create(category_params)
+    if category.valid?
+      render json: category, status: :created
+    else
+      render json: { errors: category.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    category = find_category
+    category.update(category_params)
+    render json: category
+  end
+
+  def destroy
+    category = find_category
+    category.destroy
+    render json: category 
+  end
+
+  private
+
+  def category_params
+    # params.permit(:name, :amount, :color, :start_date, :end_date, :user_id)
+  end
+
+  def find_category
+    Category.find(params[:id])
+  end
+  
+  def render_not_found_response
+    render json: { error: "Category not found" }, status: :not_found
+  end
+
 end
