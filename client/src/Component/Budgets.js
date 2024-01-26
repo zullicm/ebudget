5afebd2c804814d6816b5478c56bf66602a66629
Christopher Marzulli <chriszulli15@gmail.com>
2024-01-26 14:43:41 -0500

    redux

--- client/src/Component/Budgets.js
+++ client/src/Component/Budgets.js
@@ -12,7 +12,7 @@ function Budgets(){
   const {user, setUser} = useContext(UserContext)
   return(
     <div>
-      {budgets.map(budget =><Budget key={budget.id} childBudget={budget} />)}
+      {reduxBudgets.map(budget =><Budget key={budget.id} childBudget={budget} />)}
     </div>
   )
 }
